396cb234c45ae1747f8f4f3b25ceb1d6a52e6fd2
kristinnv12 <kristinnv12@ru.is>
2014-05-03 11:51:41 +0000

    foundation for the game

---
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/mainmenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mainmenu : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	private int buttonWidth = 200;
+	private int buttonHeight = 50;
+	private int groupWidth = 200;
+	private int groupHeigth = 170;
+
+	void OnGUI () {
+		GUI.BeginGroup (new Rect (((Screen.width / 2) - (groupWidth / 2)), ((Screen.height / 2) - (groupHeigth / 2)), groupWidth, groupHeigth));
+		if(GUI.Button(new Rect(0,0,buttonWidth,buttonHeight), "Start The Stacking"))
+		{
+			Application.LoadLevel("project");
+		}
+		if(GUI.Button(new Rect(0,60,buttonWidth,buttonHeight), "Quit"))
+		{
+			Application.Quit();
+		}
+		GUI.EndGroup();
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
