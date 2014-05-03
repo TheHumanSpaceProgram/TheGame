29f5a8c2e50aa65e61aaf320819c9162b09cefe7
gudmundurste12 <gudmundurste12@ru.is>
2014-05-03 12:46:13 +0000

    Added scripts for dragging objects, activating the end of the game and a few minor tweaks

---
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/DragMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragMovement : MonoBehaviour {
+	
+	private Vector3 screenPoint;
+	private Vector3 offset;
+	private float gravityScale;
+	
+	// Use this for initialization
+	void Start () {
+		gravityScale = rigidbody2D.gravityScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//Setup for moving the shape
+	void OnMouseDown(){
+		screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+		offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+		
+		//Makes the shape stop moving and rotating
+		rigidbody2D.gravityScale = 0;
+		rigidbody2D.velocity = new Vector2(0,0);
+		rigidbody2D.angularVelocity = 0.0f;
+	}
+	
+	//Moves the shape
+	void OnMouseDrag()
+	{
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		gameObject.transform.position = curPosition;
+	}
+	
+	//Commits the shape's position and reset it's properties
+	void OnMouseUp(){
+		rigidbody2D.gravityScale = gravityScale;
+	}
+}
---
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/OceanBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OceanBehaviour : MonoBehaviour {
+	bool verbose = true;
+	public GUIText gameOver;
+	public GameObject thePlank;
+	
+	// Use this for initialization
+	void Start () {
+		this.gameOver.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	//The game is lost
+	public void OnTriggerEnter2D(Collider2D theCollider){
+		if(!theCollider.gameObject.Equals(thePlank)){
+			if(verbose){
+				print ("Collision enter");
+			}
+			
+			this.gameOver.text = "Game Over";
+			theCollider.gameObject.audio.Play();
+		}
+	}
+}
