0e779ea43f4c50dfc2f9cc4b0316d662f56db1ec
kristinnv12 <kristinnv12@ru.is>
2014-05-06 15:06:37 +0000

    selection slides away

---
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/MoveRussianSelection.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveRussianSelection : MonoBehaviour {
+	bool MoveAway = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (MoveAway == true) {
+						if (transform.position.x < 60) {
+								transform.Translate (Vector3.right * 50 * Time.deltaTime, Space.World);
+						}
+		} else {
+			if (transform.position.x > 28.11935) { 
+				transform.Translate (Vector3.left * 50 * Time.deltaTime, Space.World);
+			}
+
+		}
+	}
+	public void away() {
+		MoveAway = true;
+	}
+	public void back(){
+		MoveAway = false;
+	}
+
+
+}
--- HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/NewObj.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/NewObj.cs
@@ -10,45 +10,65 @@ public class NewObj : MonoBehaviour {
 	private int groupHeigth = 170;
 	public GameObject theObj;
 	public GameObject instance;
-
+	public MoveRussianSelection russianSelect;
+	
 	// Use this for initialization
 	void Start () {
-
+		
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 	void OnMouseDown () {
 		if (created == false) {
 			GameObject instance = (GameObject)Instantiate(theObj, transform.position, transform.rotation);
+			instance.transform.parent = GameObject.Find("P1selection").transform;
+			instance.transform.localScale = transform.localScale;
 			created = true;
 			commited = false;
+			MoveSelectionAway();
 		}
 		
 	}
+	
 	void GiveRigid (){
 		Destroy (instance.GetComponent("DragMovement"));
 		instance.rigidbody2D.mass = 200;
 		instance.rigidbody2D.gravityScale = 1;
 		instance.rigidbody2D.isKinematic = false;
 	}
-
+	
 	void OnGUI () {
 		if (commited == false) {
 			GUI.BeginGroup (new Rect (((Screen.width / 2) - (groupWidth / 2)), ((Screen.height / 2) - (groupHeigth / 2)), groupWidth, groupHeigth));
 			if(GUI.Button(new Rect(0,0,buttonWidth,buttonHeight), "Commit"))
 			{
 				GiveRigid();
-
+				
 				commited = true;
-
+				transform.parent = null;
+				MoveSelectionOnScreen();
+				
+			}
+			if(GUI.Button(new Rect(0,60,buttonWidth,buttonHeight), "change shape"))
+			{
+				MoveSelectionOnScreen();
+				Destroy(instance);
+				
 				GameLogic.ChangePlayer();
-
+				
+				
 			}
 			GUI.EndGroup();
 		}
-
+	}
+	void MoveSelectionAway (){
+		russianSelect.away ();
+	}
+	void MoveSelectionOnScreen (){
+		russianSelect.back ();
 	}
 }
