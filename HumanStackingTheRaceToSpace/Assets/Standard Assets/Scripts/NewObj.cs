c42a0afe97da042a15a1ec6d90627b3c34d1d9f2
kristinnv12 <kristinnv12@ru.is>
2014-05-06 12:42:13 +0000

    new obj script

--- HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/NewObj.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/NewObj.cs
@@ -2,10 +2,18 @@
 using System.Collections;
 
 public class NewObj : MonoBehaviour {
-	public GameObject spawnObject;
+	private int buttonWidth = 200;
+	private int buttonHeight = 50;
+	private bool created = false;
+	private bool commited = true;
+	private int groupWidth = 200;
+	private int groupHeigth = 170;
+	public GameObject theObj;
+	public GameObject instance;
+
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
@@ -13,6 +21,30 @@ public class NewObj : MonoBehaviour {
 	
 	}
 	void OnMouseDown () {
-		Instantiate (spawnObject);
+		if (created == false) {
+			GameObject instance = (GameObject)Instantiate(theObj, transform.position, transform.rotation);
+			created = true;
+			commited = false;
+		}
+		
+	}
+	void GiveRigid (){
+		Destroy (instance.GetComponent("DragMovement"));
+		instance.rigidbody2D.mass = 200;
+		instance.rigidbody2D.gravityScale = 1;
+		instance.rigidbody2D.isKinematic = false;
+	}
+
+	void OnGUI () {
+		if (commited == false) {
+			GUI.BeginGroup (new Rect (((Screen.width / 2) - (groupWidth / 2)), ((Screen.height / 2) - (groupHeigth / 2)), groupWidth, groupHeigth));
+			if(GUI.Button(new Rect(0,0,buttonWidth,buttonHeight), "Commit"))
+			{
+				GiveRigid();
+				commited = true;
+			}
+			GUI.EndGroup();
+		}
+
 	}
 }
