b8e27de8890e9f3e56ae737b656e5e540d93875b
gudmundurste12 <gudmundurste12@ru.is>
2014-05-07 14:50:18 +0000

    Added camera movement with restrictions

---
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovement : MonoBehaviour {
+	private bool verbose = false;
+	private float movementSpeed = 0.5f;
+	private float maxUp = 134f;
+	private float maxDown = 120f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey("up")){
+			this.transform.Translate(Vector3.up * movementSpeed);
+		}
+		if(Input.GetKey("down")){
+			this.transform.Translate(Vector3.up * -1 * movementSpeed);
+		}
+
+		if(transform.position.y > maxUp){
+			Vector3 tempVector = new Vector3(transform.position.x, maxUp, transform.position.z);
+			transform.position = tempVector;
+			if(verbose){
+				print ("Reached the top");
+			}
+		}
+		if(this.transform.position.y < maxDown){
+			Vector3 tempVector = new Vector3(transform.position.x, maxDown, transform.position.z);
+			transform.position = tempVector;
+			if(verbose){
+				print ("Hit the bottom");
+			}
+		}
+	}
+}
