9401edd7dbbe03ac8f58e55bd376d7664d7c2578
kristinnv12 <kristinnv12@ru.is>
2014-05-13 12:03:07 +0000

    bug fix: selection looses the collider when you have picked an object

--- HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/MoveAmericanSelection.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/MoveAmericanSelection.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MoveAmericanSelection : MonoBehaviour {
 	public static bool MoveAway = false;
+	public bool col;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,10 +12,33 @@ public class MoveAmericanSelection : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if ((MoveAway == true) || GameLogic.gameOver) {
+			if(col)
+			{
+				col = false;
+				foreach (Transform child in transform)
+				{
+					if(child.tag.Equals("HumanObject"))
+					{
+						child.collider2D.enabled = false;
+					}
+					
+				}
+			}
 			if (transform.position.x > -70) {
 				transform.Translate (Vector3.left * 50 * Time.deltaTime, Space.World);
 			}
 		} else {
+			if(!col)
+			{
+				col = true;
+				foreach (Transform child in transform)
+				{
+					if(child.tag.Equals("HumanObject"))
+					{
+						child.collider2D.enabled = true;
+					}
+				}
+			}
 			if (transform.position.x < -39.568) { 
 				transform.Translate (Vector3.right * 50 * Time.deltaTime, Space.World);
 			}
--- HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/MoveRussianSelection.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/MoveRussianSelection.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class MoveRussianSelection : MonoBehaviour {
 	public static bool MoveAway = false;
+	public bool col = true;
 
 	// Use this for initialization
 	void Start () {
@@ -13,10 +14,34 @@ public class MoveRussianSelection : MonoBehaviour {
 	void Update () {
 
 		if ((MoveAway == true) || GameLogic.gameOver) {
+			if(col)
+			{
+				col = false;
+				foreach (Transform child in transform)
+				{
+					if(child.tag.Equals("HumanObject"))
+					{
+						child.collider2D.enabled = false;
+					}
+
+				}
+			}
+
 						if (transform.position.x < 60) {
 								transform.Translate (Vector3.right * 50 * Time.deltaTime, Space.World);
 						}
 		} else {
+			if(!col)
+			{
+				col = true;
+				foreach (Transform child in transform)
+				{
+					if(child.tag.Equals("HumanObject"))
+					{
+						child.collider2D.enabled = true;
+					}
+				}
+			}
 			if (transform.position.x > 28.11935) { 
 				transform.Translate (Vector3.left * 50 * Time.deltaTime, Space.World);
 			}
