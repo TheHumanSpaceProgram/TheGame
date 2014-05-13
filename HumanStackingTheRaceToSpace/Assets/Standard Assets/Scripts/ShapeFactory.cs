d9dab3f83d217dea6d4b2a95cec58ddfddc54225
gudmundurste12 <gudmundurste12@ru.is>
2014-05-13 13:42:31 +0000

    Removed dead code

--- HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/ShapeFactory.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/ShapeFactory.cs
@@ -6,27 +6,6 @@ public class ShapeFactory : MonoBehaviour {
 	public static System.Random RandomGenerator;
 	private static int NumberOfShapes;
 
-	static ShapeFactory(){
-		/*
-		RandomGenerator = new System.Random();
-		NumberOfShapes = 4;
-		ShapeLists = new GameObject[2][];
-		ShapeLists[0] = new GameObject[NumberOfShapes];
-		ShapeLists[1] = new GameObject[NumberOfShapes];
-
-		//Assigning the shapes here because the class is static
-		ShapeLists[0][0] = GameObject.Find ("Proto1Circle");
-		ShapeLists[0][1] = GameObject.Find ("Proto1Pentagon");
-		ShapeLists[0][2] = GameObject.Find ("Proto1Rectangle");
-		ShapeLists[0][3] = GameObject.Find ("Proto1Triangle");
-
-		ShapeLists[1][0] = GameObject.Find ("Proto2Circle");
-		ShapeLists[1][1] = GameObject.Find ("Proto2Pentagon");
-		ShapeLists[1][2] = GameObject.Find ("Proto2Rectangle");
-		ShapeLists[1][3] = GameObject.Find ("Proto2Triangle");
-		*/
-	}
-
 	public void Start(){
 		RandomGenerator = new System.Random();
 		NumberOfShapes = 4;
