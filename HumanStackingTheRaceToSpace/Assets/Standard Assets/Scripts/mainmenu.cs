bc915113a154b2830ff6d44292fd4b6b0b8368d5
kristinnv12 <kristinnv12@ru.is>
2014-05-16 12:10:44 +0000

    singleplayer mode added to main menu

--- HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/mainmenu.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/mainmenu.cs
@@ -10,19 +10,23 @@ public class mainmenu : MonoBehaviour {
 	private int buttonWidth = 200;
 	private int buttonHeight = 50;
 	private int groupWidth = 200;
-	private int groupHeigth = 170;
+	private int groupHeigth = 250;
 
 	void OnGUI () {
 		GUI.BeginGroup (new Rect (((Screen.width / 2) - (groupWidth / 2)), ((Screen.height / 2) - (groupHeigth / 2)), groupWidth, groupHeigth));
 		if(GUI.Button(new Rect(0,0,buttonWidth,buttonHeight), "Start The Stacking"))
 		{
 			Application.LoadLevel("humanStacking");
-		}		
-		if(GUI.Button(new Rect(0,60,buttonWidth,buttonHeight), "How To Play"))
+		}
+		if(GUI.Button(new Rect(0,60,buttonWidth,buttonHeight), "Single Player"))
+		{
+			Application.LoadLevel("singleplayer");
+		}
+		if(GUI.Button(new Rect(0,120,buttonWidth,buttonHeight), "How To Play"))
 		{
 			Application.LoadLevel("tutorial");
 		}
-		if(GUI.Button(new Rect(0,120,buttonWidth,buttonHeight), "Quit"))
+		if(GUI.Button(new Rect(0,180,buttonWidth,buttonHeight), "Quit"))
 		{
 			Application.Quit();
 		}
