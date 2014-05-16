df6f01ec1248cc2aabfdb51381aa22872a9229d3
gudmundurste12 <gudmundurste12@ru.is>
2014-05-16 10:36:13 +0000

    Tweaked the points for the shapes

--- HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/GameLogic.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/GameLogic.cs
@@ -449,6 +449,7 @@ public class GameLogic : MonoBehaviour {
 	private static void OnTimedEvent(object source, ElapsedEventArgs e)
 	{
 		_Time++;				
+		print ("OnTimedEvent. _Time: " + _Time);
 	}
 
 	private static void OnWaitTimedEvent(object source, ElapsedEventArgs e)
