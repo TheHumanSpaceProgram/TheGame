07bb18a55e884cba9f497eba6e77fd3fb1cc4d9a
kristinnv12 <kristinnv12@ru.is>
2014-05-17 12:10:28 +0000

    fixing intros, made rematch button, added national anthems to end scene

--- HumanStackingTheRaceToSpace/Assets/Standard Assets/ScoreBoard/winnerboard.cs
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/ScoreBoard/winnerboard.cs
@@ -6,10 +6,18 @@ public class winnerboard : MonoBehaviour {
 	public GUIText playerOneGUItxt;
 	public GUIText playerOneScore;
 	public static bool WinnerScreen = false;
+	public AudioClip sovietAnthem;
+	public AudioClip USAAnthem;
 
 
 	// Use this for initialization
 	void Start () {
+		if (GameLogic._playerTurn.PlayerName == "The USA") {
+			AudioSource.PlayClipAtPoint(USAAnthem, transform.position);
+				} else {
+			AudioSource.PlayClipAtPoint(sovietAnthem, transform.position);
+		}
+
 		WinnerScreen = true;
 		playerOneGUItxt.text = GameLogic._playerTurn.PlayerName + " WINS";
 		playerOneScore.text = "SCORE: " + GameLogic._playerTurn.PlayerCurrentScore;
---
+++ HumanStackingTheRaceToSpace/Assets/Standard Assets/Scripts/RestartButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private int buttonWidth = 200;
+	private int buttonHeight = 50;
+	private int groupWidth = 200;
+	private int groupHeigth = 320;
+	
+	void OnGUI () {
+		GUI.BeginGroup (new Rect (((Screen.width / 2) - (groupWidth / 2)), ((Screen.height / 2) - (groupHeigth / 2)), groupWidth, groupHeigth));
+		if(GUI.Button(new Rect(0,240,buttonWidth,buttonHeight), "Rematch"))
+		{
+			Application.LoadLevel("humanStacking");
+		}
+		GUI.EndGroup();
+	}
+}
